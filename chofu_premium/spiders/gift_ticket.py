2c4baa417d28fc4b0b09beaf6079435ff440f393
devs@outlook.jp <devs@outlook.jp>
2020-10-17 16:09:11 +0900

    add single page crawler

--- chofu_premium/spiders/gift_ticket.py
+++ chofu_premium/spiders/gift_ticket.py
@@ -8,5 +8,18 @@ class GiftTicketSpider(scrapy.Spider):
     start_urls = ['https://premium-gift.jp/chofu/use_store?events=page&id=1']
 
     def parse(self, response: HtmlResponse, **kwargs):
-        text = response.xpath("//div[@class='store-card__item']/h3/text()").extract()
-        [print(item) for item in text]
+        table = response.xpath("//div[@class='store-card__item']")
+        for item in table:
+            store_title = item.xpath(".//h3[@class='store-card__title']/text()")
+            store_tag = item.xpath(".//p[@class='store-card__tag']/text()")
+            store_table = item.xpath(".//table[@class='store-card__table']/tbody/tr/td")
+            store_address = store_table[0].xpath("string(.)")
+            store_tel = store_table[1].xpath("string(.)")
+            store_url = store_table[2].xpath(".//a/@href")
+
+
+            print(f"店名：{store_title.get()}\n"
+                  f"タグ：{store_tag.get()}\n"
+                  f"住所：{store_address.get()}\n"
+                  f"電話：{store_tel.get()}\n"
+                  f"URL：{store_url.get()}\n")
